b1062b2e3b665f2ae37805d2b569f68fd7aca55f
Pierre-yves BILLETTE <pbillett@e3r4p13.42.fr>
2016-02-02 20:21:19 +0100

    correct check inputfile

--- checkfile.c
+++ checkfile.c
@@ -6,7 +6,7 @@
 /*   By: pbillett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/02 16:32:42 by pbillett          #+#    #+#             */
-/*   Updated: 2016/02/02 19:35:11 by pbillett         ###   ########.fr       */
+/*   Updated: 2016/02/02 20:19:59 by pbillett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,10 @@ int			checkfile(char* filename)
 	int ret;
 	char buf[BUF_SIZE + 1];
 	int compt;
+	int checkcontent;
 
-	compt = 0;
+	checkcontent = 0;
+	compt = 1;
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
@@ -31,24 +33,41 @@ int			checkfile(char* filename)
 		return (1);
 	}
 	// do something like show content
-	ft_putstr("ok file open\n");
 	while ((ret = read(fd, buf, BUF_SIZE)))
 	{
 		buf[ret] = '\0';
 		//ft_putnbr(ret);
 		//ft_putchar('\n');
-		ft_putstr(buf);
+		// POUR AFFICHER LE CONTENU DU FICHIER (VIA LE BUFFER):
+		//ft_putstr(buf);
 		//ft_putstr("end\n");
+
+		// PAS DE CARACTERE DIFFERENTS DE \n . et #
+		if (buf[0] != '\n' && buf[0] != '.' && buf[0] != '#')
+			checkcontent = 1;
+
+		// PAS 2 LIGNE VIDES D'AFFILE
+
+		// PAS DE TETRIMINO QUI NE TOUCHE AUCUN DES COTES (soit la forme du carre au centre)
+
+		// PAS DE LIGNE DE PLUS DE 4 CARACTERES
 		if (buf[0] == '\n')
 		{
 			if (compt != 5)
-				ft_putnbr(compt);
-				ft_putstr("error on line\n");
-			//ft_putstr("sp\n");
+			{
+				if (compt != 1)
+					checkcontent = 1;
+					//ft_putnbr(compt);
+					//ft_putstr("error\n");
+			}
 			compt = 0;
 		}
 		compt++;
 	}
+	if (checkcontent == 0)
+		ft_putstr("ok file open\n");
+	else
+		ft_putstr("error\n");
 
 	if (close(fd))
 	{
