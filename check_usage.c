59161bfb5f1aabc4797b5f5d9159cfe042fb7ca4
Pierre-yves BILLETTE <pbillett@e2r7p15.42.fr>
2016-06-03 20:19:45 +0200

    Modif usage for not existing file -> to more simple

--- check_usage.c
+++ check_usage.c
@@ -6,7 +6,7 @@
 /*   By: pbillett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/02 18:36:51 by pbillett          #+#    #+#             */
-/*   Updated: 2016/05/02 18:36:56 by pbillett         ###   ########.fr       */
+/*   Updated: 2016/06/03 20:14:52 by pbillett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@ int	check_usage(int argc, char **argv, int nombrepieces)
 	{
 		if (openfile(argv[1]) == NULL)
 		{
-			ft_putstr("fillit: impossible d'évaluer «");
-			ft_putstr(argv[1]);
-			ft_putstr("»: Aucun fichier ou dossier de ce type\n");
+			ft_putstr("usage: ./fillit input_file\n");
 			return (1);
 		}
 		if (checkfile(openfile(argv[1]), &nombrepieces) != NULL)
